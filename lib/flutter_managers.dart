89d51da188d6503a73e83b9ffd66b386ea773df1
Oğuzhan Küçüksaraç <55416005+Oguzhanks@users.noreply.github.com>
2022-06-14 21:17:02 +0300

    feat: add export

--- lib/flutter_managers.dart
+++ lib/flutter_managers.dart
@@ -2,3 +2,6 @@ library flutter_managers;
 
 export 'src/managers/cache/cache_manager.dart';
 export 'src/managers/client/client_manager.dart';
+export 'src/models/client_model.dart';
+export 'src/models/response_model.dart';
+export 'src/models/error_model.dart';
